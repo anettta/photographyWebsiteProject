0e4a6871d582048bb609327b886e982c0760e9a8
Anya <annagapyuk@gmail.com>
2020-10-01 14:28:31 -0400

    updated

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -14,7 +14,7 @@ export default function CartItem({item,value}) {
                 src={images}
                 style={{width: "5rem", height: "5rem"}}
                 className="img-fluid"
-                alt=""
+                alt="chosen photograph"
                 />
             </div>
             <div className="col-10 mx-auto col-lg-2">
